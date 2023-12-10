67588319870293d8ceff448a99f1279249b6f5ae
Christian Schwerdt <christianjosef@hotmail.com>
2023-12-10 21:06:46 +0100

    added dark setting detection logic and refactored

--- scripts/ingest_video_data.py
+++ scripts/ingest_video_data.py
@@ -1,29 +1,42 @@
 import moviepy.editor as moviepy
 import uuid
 import sqlite3 
+import ingest_video_data_help_methods as helper
 
+# create DB
 sqliteConnection = sqlite3.connect('vdms_meta_data.db')
 cursor = sqliteConnection.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS video_metadata
-                  (filename TEXT, description TEXT, duration REAL)
+                  (filename TEXT, description TEXT, duration REAL, dark_setting TEXT)
                """)
 sqliteConnection.commit()
 
-
+# get manual metadata input
 print("Please specify the filepath the video you like to store in the VDMS:")
 filepath = input()
 print("Please describe the video:")
 description = input()
 
+# convert and save file with UUID
 clip = moviepy.VideoFileClip(filepath)
-duration = clip.duration
 filename = str(uuid.uuid4())
 clip.write_videofile(f"video_data_catalog/{filename}.mp4")
 
-cursor.execute("INSERT INTO video_metadata (filename, description, duration) VALUES (?, ?, ?)",(filename, description, duration))
+# get automatic metadata
+helper.darkSetting(filepath)
+duration = clip.duration
+
+
+# save video metadata in DB 
+cursor.execute("INSERT INTO video_metadata (filename, description, duration, dark_setting) VALUES (?, ?, ?, ?)",(filename, description, duration, video_dark_setting))
 sqliteConnection.commit()
+
+
+
+
 
 cursor.execute("SELECT * FROM video_metadata")
 print(cursor.fetchall())
+
 
 #./raw_data/IMG_0881.MOV
---
+++ scripts/ingest_video_data_help_methods.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+def darkSetting(filepath: str):
+    cap = cv2.VideoCapture(filepath)
+    nbr_frames=0
+    class_sum = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        im_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        meanpercent = np.mean(im_gray) * 100 / 255
+        frame_classification = 1 if meanpercent > 50 else 0
+        class_sum = class_sum + frame_classification
+        nbr_frames += 1
+
+    video_dark_setting = "Dark" if class_sum/nbr_frames < 0.5 else "light"
+    return video_dark_setting
