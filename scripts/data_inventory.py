b0446eaac30a74478ce77550e3f9c6a01f9a3c37
Christian Schwerdt <christianjosef@hotmail.com>
2023-12-16 17:08:32 +0100

    refactoring

--- scripts/data_inventory.py
+++ scripts/data_inventory.py
@@ -1,45 +1,13 @@
-import sqlite3
-import matplotlib.pyplot as plt
+from data_inventory_help_methods import connectToDB, getTotalNbrOfVideos, getVideoLightSettings, nbrPeopleInVideos, durationOfVideos, plot_data
 
-# Connect to DB
-sqliteConnection = sqlite3.connect('vdms_meta_data.db')
-cursor = sqliteConnection.cursor()
+cursor = connectToDB('vdms_meta_data.db')
 
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata")
-total_nbr_videos = cursor.fetchall()[0][0]
+total_nbr_videos =  getTotalNbrOfVideos(cursor)
+video_light_settings = getVideoLightSettings(cursor, total_nbr_videos)
+nbr_people_in_videos = nbrPeopleInVideos(cursor)
+duration_of_videos = durationOfVideos(cursor)
 
-
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'dark'")
-nbr_dark_videos = cursor.fetchall()[0][0]
-
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'light'")
-nbr_light_video = cursor.fetchall()[0][0]
-
-light_setting_data = {'dark': nbr_dark_videos, 'light': nbr_light_video, 'other': total_nbr_videos-nbr_dark_videos-nbr_light_video}
-
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE nbr_people = 0")
-nbr_people_0 = cursor.fetchall()[0][0]
-
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE nbr_people = 1")
-nbr_people_1 = cursor.fetchall()[0][0]
-
-cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE nbr_people = 2")
-nbr_people_2 = cursor.fetchall()[0][0]
-
-nbr_people_data = {'0': nbr_people_0, '1': nbr_people_1, '2': nbr_people_2}
-
-cursor.execute(f"SELECT duration FROM video_metadata")
-video_durations = cursor.fetchall()
-video_durations = [durations[0] for durations in video_durations]
-
-
-fig, ax = plt.subplots(3,1)
-
-fig.suptitle(f"Total number of videos: {total_nbr_videos}")
-ax[0].bar(x=list(light_setting_data.keys()), height=list(light_setting_data.values()))
-ax[1].bar(x=list(nbr_people_data.keys()), height=list(nbr_people_data.values()))
-ax[2].hist(video_durations, bins = range(0, 10, 1))
-ax[0].set_title("Distribution of dark and light setting")
-ax[1].set_title("Distribution of number of people in videos")
-ax[2].set_title("Distribution of time duration of videos")
-plt.show()
+plot_data(total_nbr_videos,
+    video_light_settings,
+    nbr_people_in_videos,
+    duration_of_videos)
---
+++ scripts/data_inventory_help_methods.py
@@ -0,0 +1,71 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+def connectToDB(db_name):
+    sqliteConnection = sqlite3.connect(db_name)
+    return sqliteConnection.cursor()
+
+
+def getTotalNbrOfVideos(cursor):
+    cursor.execute(f"SELECT COUNT(*) FROM video_metadata")
+    return cursor.fetchall()[0][0]
+
+
+def getVideoLightSettings(cursor, total_nbr_videos):
+    cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'dark'")
+    nbr_videos_dark = cursor.fetchall()[0][0]
+
+    cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'light'")
+    nbr_video_light = cursor.fetchall()[0][0]
+
+    nbr_other_video = total_nbr_videos-nbr_videos_dark-nbr_video_light
+
+    return {'dark': nbr_videos_dark, 'light': nbr_video_light, 'other': nbr_other_video}
+
+
+def getNbrDarkVideos(cursor):
+    cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'dark'")
+    return cursor.fetchall()[0][0]
+
+
+def getNbrLightVideos(cursor):
+    cursor.execute(f"SELECT COUNT(*) FROM video_metadata WHERE dark_setting LIKE 'light'")
+    return cursor.fetchall()[0][0]
+
+
+def nbrPeopleInVideos(cursor):
+    cursor.execute(f"SELECT nbr_people FROM video_metadata")
+    nbr_people_in_videos = cursor.fetchall()
+    return [nbr_people[0] for nbr_people in nbr_people_in_videos]
+
+
+def durationOfVideos(cursor):
+    cursor.execute(f"SELECT duration FROM video_metadata")
+    video_durations = cursor.fetchall()
+    return [durations[0] for durations in video_durations]
+
+
+def getFpsOfVideos(cursor):
+    cursor.execute(f"SELECT fps FROM video_metadata")
+    fps_of_videos = cursor.fetchall()
+    return [fps_of_video[0] for fps_of_video in fps_of_videos]
+
+
+def plot_data(total_nbr_videos,
+    video_light_settings,
+    nbr_people_in_videos,
+    duration_of_videos,
+    fps_of_videos):
+    
+    fig, ax = plt.subplots(2,2)
+    fig.suptitle(f"Total number of videos: {total_nbr_videos}")
+    ax[0,0].bar(x=list(video_light_settings.keys()), height=list(video_light_settings.values()))
+    ax[1,0].hist(nbr_people_in_videos, bins = range(1, 10, 1))
+    ax[0,1].hist(duration_of_videos, bins = range(0, 10, 1))
+    ax[1,1].hist(fps_of_videos, bins = range(20, 40, 1))
+    
+    ax[0,0].set_title("Distribution of dark and light setting")
+    ax[1,0].set_title("Distribution of number of people in videos")
+    ax[0,1].set_title("Distribution of time duration of videos")
+    ax[1,1].set_title("Distribution of fps of videos")
+    plt.show()
