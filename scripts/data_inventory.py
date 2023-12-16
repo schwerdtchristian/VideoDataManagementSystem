02670d66fe6a31811b22848722d72c089efcc054
Christian Schwerdt <christianjosef@hotmail.com>
2023-12-16 16:30:08 +0100

    Added time duration of videos to plots

--- scripts/data_inventory.py
+++ scripts/data_inventory.py
@@ -28,11 +28,18 @@ nbr_people_2 = cursor.fetchall()[0][0]
 
 nbr_people_data = {'0': nbr_people_0, '1': nbr_people_1, '2': nbr_people_2}
 
-fig, ax = plt.subplots(2,1)
+cursor.execute(f"SELECT duration FROM video_metadata")
+video_durations = cursor.fetchall()
+video_durations = [durations[0] for durations in video_durations]
+
+
+fig, ax = plt.subplots(3,1)
 
 fig.suptitle(f"Total number of videos: {total_nbr_videos}")
 ax[0].bar(x=list(light_setting_data.keys()), height=list(light_setting_data.values()))
 ax[1].bar(x=list(nbr_people_data.keys()), height=list(nbr_people_data.values()))
+ax[2].hist(video_durations, bins = range(0, 10, 1))
 ax[0].set_title("Distribution of dark and light setting")
 ax[1].set_title("Distribution of number of people in videos")
+ax[2].set_title("Distribution of time duration of videos")
 plt.show()
